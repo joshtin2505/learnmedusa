ffdcd944730c3f659dd030fefff1c14058ae3392
Justin Castro Perez <justin.castro@estela.com>
2025-06-06 14:25:51 -0500

    Eliminar la integración de MercadoPago: se ha removido el plugin de MercadoPago de la configuración de Medusa y se ha actualizado package.json para reflejar este cambio.

--- medusa-config.ts
+++ medusa-config.ts
@@ -19,14 +19,4 @@ module.exports = defineConfig({
       resolve: "./src/modules/brand",
     },
   ],
-  plugins: [
-    {
-     resolve: "@minskylab/medusa-payment-mercadopago" ,
-      options:{
-        access_token: process.env.MERCADOPAGO_ACCESS_TOKEN,
-        success_backurl: process.env.MERCADOPAGO_SUCCESS_BACKURL,
-        webhook_url: process.env.MERCADOPAGO_WEBHOOK_URL,
-      }
-    }
-  ]
 });
